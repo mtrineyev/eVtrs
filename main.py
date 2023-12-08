87c9adcafde86a3f59f4ec80cf51b5a9f3933ee2
Maksym Trineiev <61587413+mtrineyev@users.noreply.github.com>
2023-12-09 01:26:54 +0200

    import Flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.bank_account import BankAccount
 from src.employee import PayrollManager
 from src.figures import Rectangle, Circle
+from src.flat import Flat
 
 
 if __name__ == "__main__":
